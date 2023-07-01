3098d108dd2affc98e939662f860acfd37410ec8
grombor <gromgaming@yahoo.com>
2023-07-01 08:17:01 +0200

    upgraded accordion

--- app/components/molecues/AccordionComponent.jsx
+++ app/components/molecues/AccordionComponent.jsx
@@ -5,33 +5,33 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Checkbox,
 } from '@chakra-ui/react';
 import React from 'react';
 
 const AccordionComponent = ({ props }) => {
   return (
     <Accordion allowToggle>
-       {Array.isArray(props) &&
+      {Array.isArray(props) &&
         props.map((item) => {
-        return (
-          <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                {item.name}
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <p>Address: {item.url}</p>
-            <p>Selector: {item.selector}</p>
-          </AccordionPanel>
-        </AccordionItem>
-        )
-      })}
-
-
+          return (
+            <AccordionItem key={item.name}>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    <Checkbox px={'1em'}>&nbsp;</Checkbox>
+                    {item.name}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <p>Address: {item.url}</p>
+                <p>Selector: {item.selector}</p>
+              </AccordionPanel>
+            </AccordionItem>
+          );
+        })}
     </Accordion>
   );
 };
