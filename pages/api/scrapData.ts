ec695e00f12d00753fdef307786cdccac0a1792c
grombor <gromgaming@yahoo.com>
2023-07-11 22:15:25 +0200

    refactored scraper fn

--- pages/api/scrapData.ts
+++ pages/api/scrapData.ts
@@ -1,32 +1,10 @@
-/**
- * Scrapuje dane z podanej strony internetowej na podstawie dostarczonych selektorów.
- * @param {Object} req - Obiekt reprezentujący żądanie HTTP.
- * @param {Object} res - Obiekt reprezentujący odpowiedź HTTP.
- * @returns {Promise<void>} - Promise bez wartości zwracanej.
- * @throws {Error} - Jeśli wystąpi błąd podczas scrapowania danych.
- */
-
-
 import { scrapeValueFromWebsite } from '../scraper';
 
 export default async function scrapData(req, res) {
   try {
     const { url, selectors } = req.body;
 
-    const results = await Promise.all(
-      selectors.map(async (selector) => {
-        const { uuid, name, selector: selectorString, isChecked } = selector;
-        const value = await scrapeValueFromWebsite(url, selectorString);
-
-        return {
-          uuid,
-          name,
-          selector: selectorString,
-          isChecked: Boolean(isChecked),
-          value,
-        };
-      })
-    );
+    const results = await scrapeValueFromWebsite(url, selectors)
 
     const dataResult = {
       url,
--- pages/scraper.ts
+++ pages/scraper.ts
@@ -1,13 +1,26 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-export async function scrapeValueFromWebsite(url, selector) {
+export async function scrapeValueFromWebsite(url, selectors) {
   try {
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
-    const value = $(selector).text();
-    return value;
+
+    const results = selectors.map((selector) => {
+      const { uuid, name, selector: selectorString, isChecked } = selector;
+      const value = $(selectorString).text();
+
+      return {
+        uuid,
+        name,
+        selector: selectorString,
+        value,
+        isChecked: Boolean(isChecked),
+      };
+    });
+
+    return results;
   } catch (error) {
     console.error(error);
     return null;
