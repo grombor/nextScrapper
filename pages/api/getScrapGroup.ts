ddd2753b0a6db4d0759df945466dab176f532f18
grombor <gromgaming@yahoo.com>
2023-07-14 16:25:49 +0200

    updated tasks

--- pages/api/getScrapGroup.ts
+++ pages/api/getScrapGroup.ts
@@ -1,26 +1,17 @@
-/**
- * Zwraca grupę o identyfikatorze UUID.
- *
- * @param {string} uuid Identyfikator UUID grupy.
- * @returns {Response} Odpowiedź HTTP z grupą.
- */
+// TODOS:
+// - uruchomienie fukcji scrapujacej dla kazdego elementu w grupie
+// - przypisanie wyniiku fukcji scrapujaej dla parametru value
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { data } from '../data';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    if (req.method !== 'GET') {
-      return res
-        .status(405)
-        .json({ error: 'Metoda żądania nie jest obsługiwana' });
-    }
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { results } = req.body;
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Błąd serwera' });
+    console.log(results)
+
+    return res.status(200).json(results);
   }
+
+  return res.status(405).json({ error: 'Metoda żądania nie jest obsługiwana' });
 }
