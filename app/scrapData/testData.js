f9b6e0e16e31ab23285bdcabc6ec07a59f3e7759
grombor <gromgaming@yahoo.com>
2023-07-16 18:45:40 +0200

    fixed scrappage

--- app/scrapData/testData.js
+++ app/scrapData/testData.js
@@ -2,7 +2,7 @@ const testData = {
     uuid: "d039a4cb-c61d-4b78-b689-d3ac8d1dd188",
     name: "Site XYZ",
     createdDate: "2023-07-14T11:15:00Z",
-    lastModifiedDate: "2023-07-14T11:15:00Z",
+    lastModifiedDate: "7/16/2023, 3:57:11 PM",
     isChecked: false,
     author: "Bard",
     scraps: [
@@ -48,7 +48,7 @@ const testData = {
             uuid: "e9f5822a-3e62-42db-bb20-6ab1c53d875c",
             name: "model",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.description1.rwd-320-hide > div.col-sm-9 > div",
-            value: "",
+            value: "SUPE 300-02",
             isChecked: true
           },
           {
@@ -69,35 +69,35 @@ const testData = {
             uuid: "90f9c1b2-7084-4d64-b5d7-66ed2327e5e3",
             name: "shopPriceNett",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.price > div.netto.nettoPrice > span",
-            value: "",
+            value: "469",
             isChecked: true
           },
           {
             uuid: "d14ff67f-557d-42f1-8a14-167915f1fbad",
             name: "productHeight",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.description3 > div.col-sm-8 > div.row.properties > div > div > div:nth-child(1) > div:nth-child(4) > div.col-xs-5.value",
-            value: "",
+            value: "1800",
             isChecked: true
           },
           {
             uuid: "e47fabb0-6aae-4f7a-8fd3-58b3b2b6418f",
             name: "productWidth",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.description3 > div.col-sm-8 > div.row.properties > div > div > div:nth-child(1) > div:nth-child(1) > div.col-xs-5.value",
-            value: "",
+            value: "600",
             isChecked: true
           },
           {
             uuid: "c58d8cbf-b654-41ce-a536-9fc82599fc5d",
             name: "productDepth",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.description3 > div.col-sm-8 > div.row.properties > div > div > div:nth-child(2) > div:nth-child(1) > div.col-xs-5.value",
-            value: "",
+            value: "480",
             isChecked: true
           },
           {
             uuid: "758ce536-829f-4b55-a1f5-0f0e349c5371",
             name: "productFeatures",
             selector: "#opis",
-            value: "",
+            value: "Opis i dane Szafka bhp pracownicza SUPE300-02 Szafka bhp SUPE300-02 to najpopularniejszy element wyposażenia szatni w zakładach pracy, szkołach, obiektach sportowych itp. Szafki pracownicze tego typu pozwalają pracownikom na przechowywanie w osobnych komorach odzieży roboczej oraz odzieży wierzchniej i osobistej. Podstawowe parametry szafki SUPE300-02: konstrukcja szafki bhp zgrzewana z wysokiej jakości blachy stalowej doskonałe parametry mechaniczne i antykorozyjne powłoki lakierniczej gładka, łatwa do utrzymania w czystości, powierzchnia szafki wyposażenie standardowe: wzmocnione drzwi (otwierane w kierunku prawe/lewe), otwory wentylacyjne zapewniające prawidłową cyrkulację powietrza, stała półka (światło półki - 250), drążek z 2 haczykami na ubrania, praktyczny wizytownik na drzwiach, zamek cylindryczny 1 pkt. w systemie Master (możliwość otwarcia wszystkich zamków danej serii specjalnym kluczem Master – zamawiany odrębnie), w komplecie 2 klucze standardowe, regulatory umożliwiające poziomowanie. Uniwersalna szafka pracownicza SUPE300-02 oferowana wersja szafy bhp jest przystosowana zarówno do ustawienia na podłodze, jak i montażu szafy na ławkę szafka malowana proszkowo standardowy kolor: popielaty RAL 7035 (inne kolory z palety za dopłatą 5%) Szafki pracownicze - najważniejsza jest jakość Oferowane w naszym sklepie szafki pracownicze to najwyższej jakości meble metalowe wyróżnione godłem \"Najlepsze w Polsce. Nasze szafy bhp produkowane są w nowoczesnym zakładzie produkcji mebli w podpoznańskim Bolechowie, gdzie szczególną uwagę zwracamy na jakość produkowanych wyrobów. Potwierdzeniem jakości jest także 84 miesięczny okres gwarancji jaki udzielamy na tego typu szafki bhp. Szafki bhp - komfort pracownika Szafki bhp inaczej nazywane pracowniczymi szafami ubraniowymi są niezbędnym elementem wyposażenia każdej szatni w różnego rodzaju przedsiębiorstwach, jednocześnie zwiększają komfort pracy użytkujących je pracowników. Parametry Typ: SUPE 300-02 Gwarancja: 7 lat Grubość: 0,6 Rodzaj konstrukcji / materiał: stalowa blacha Typ zamka: zamek cylindryczny Szerokość: 600 Wersja: standard Liczba komór: 2 Wysokość: 1800 Głębokość: 480 Linia: SUPE 300 Kolor frontów: popielaty RAL 7035 Kolor konstrukcji/korpusu: popielaty RAL 7035 Montaż: Dostarczane przez nas meble stalowe nie wymagają samodzielnego montażu, są gotowe do użytku. Warunki dostawy: Jeśli zamówione ilości i gabaryty mebli pozwalają na dostawę kurierską, to transport odbywa się na paletach drewnianych. W przeciwnym razie dostawa odbywa się samochodami spedycji i meble nie są wówczas umieszczone na paletach. Za rozładunek i wniesienie do budynku odpowiada Kupujący. W celu ułatwienia rozładunku (w przypadku zakupu większych ilości mebli) istnieje możliwość zamówienia pojazdu z windą lub umieszczenia mebli na dodatkowo płatnych paletach. Klucze znajdują się w osobnych opakowaniach przyklejonych na górze szafy. Prosimy dokładnie obejrzeć dostarczony towar przy przewoźniku.",
             isChecked: true
           },
           {
@@ -111,24 +111,27 @@ const testData = {
             uuid: "d799d5b4-3ef5-4986-bd67-6b6768c8e81d",
             name: "leadTime",
             selector: "#main > div.container.wrapper > div:nth-child(6) > div.container-main > div.row.main-content-2 > div.col-sm-8.product > div.row.description3 > div.col-sm-4 > div.row.availability > span",
-            value: "",
+            value: "towar w magazynie",
             isChecked: true
           },
           {
             uuid: "32a6a0f9-d3b6-4a11-8659-33b09b0f97a9",
             name: "productWarranty",
             selector: "#opis > div.col-sm-12 > div > div > div > div:nth-child(1) > div > div:nth-child(2) > div.col-xs-5.value",
-            value: "",
+            value: "7 lat",
             isChecked: true
           },
           {
             uuid: "1d7e9af9-16b6-4d33-bc54-1798b0a32e20",
             name: "comment",
             selector: "#opis > div.col-sm-12 > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div.col-xs-5.value",
-            value: "",
+            value: "SUPE 300",
             isChecked: true
           }
         ]
       }
     ]
   };
+  
+  export default testData;
+  
