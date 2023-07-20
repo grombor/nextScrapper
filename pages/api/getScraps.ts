ee5d53589e80b23cf73358d2c8b1ef78affc7dd0
grombor <gromgaming@yahoo.com>
2023-07-20 07:47:19 +0200

    added getScraps endpoint

--- pages/api/getScraps.ts
+++ pages/api/getScraps.ts
@@ -9,22 +9,25 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { id } = req.query;
-    if (!id) {
-      return res.status(400).json({ error: 'No scrap id provided for deletion.' });
+    const { data: data } = req.body;
+    if (!data || !Array.isArray(data)) {
+      return res.status(400).json({ error: 'Invalid or missing array of scrap ids to delete.' });
     }
 
-    const scrap = await prisma.scrap.findUnique({
+    // Find all Scrap entries with the provided ids
+    const scraps = await prisma.scrap.findMany({
       where: {
-        id: String(id),
+        id: {
+          in: data.map(String),
+        },
       },
     });
 
-    if (!scrap) {
-      return res.status(404).json({ error: 'Scrap with the provided id does not exist.' });
+    if (scraps.length === 0) {
+      return res.status(404).json({ error: 'No scraps found with the provided ids.' });
     }
 
-    return res.status(200).json({ data: scrap });
+    return res.status(200).json({ data: scraps });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Server error.' });
