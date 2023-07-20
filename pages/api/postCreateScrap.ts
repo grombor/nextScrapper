0c2f6424d0b02d27db4958959f8bb3f53e3b9f89
grombor <gromgaming@yahoo.com>
2023-07-20 07:33:15 +0200

    added create Scrap

--- pages/api/postCreateScrap.ts
+++ pages/api/postCreateScrap.ts
@@ -1,19 +1,70 @@
-// api/createScrap.js
+// api/createScrap.ts
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface SelectorInput {
+  uuid: string;
+  name: string;
+  selector: string;
+  value: string;
+  isChecked: boolean;
+  scrapId: string;
+}
+
+interface ScrapData {
+  uuid: string;
+  name: string;
+  createdDate: string;
+  lastModifiedDate: string;
+  isChecked: boolean;
+  author: string;
+  scraps: {
+    url: string;
+    selectors: SelectorInput[];
+  }[];
+}
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'The request method is not supported.' });
+    return res.status(405).json({ error: 'Method not allowed.' });
   }
 
   try {
-    const { name } = req.body;
+    const {
+      name,
+      createdDate,
+      lastModifiedDate,
+      isChecked,
+      author,
+      scraps,
+    } = req.body as ScrapData;
+
+    // Create a new Scrap with data from req.body
     const newScrap = await prisma.scrap.create({
       data: {
         name,
+        createdDate: new Date(createdDate),
+        lastModifiedDate: new Date(lastModifiedDate),
+        isChecked,
+        author,
       },
+    });
+
+    // Map scraps to an array with additional information about Scrap
+    const selectorData: SelectorInput[] = [];
+    scraps.forEach((scrap) => {
+      scrap.selectors.forEach((selector) => {
+        selectorData.push({
+          ...selector,
+          scrapId: newScrap.id, // Add scrapId to each selector
+        });
+      });
+    });
+
+    // Add Selectors to Scrap using createMany in prisma
+    await prisma.selector.createMany({
+      data: selectorData,
     });
 
     return res.status(201).json(newScrap);
